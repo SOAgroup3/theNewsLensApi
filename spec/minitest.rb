593b818317c9fd6335e7a72b0094899e980a7313
peyruchao <happypeggy8138@gmail.com>
2014-10-23 10:11:25 +0800

    spec v1

--- spec/minitest.rb
+++ spec/minitest.rb
@@ -6,4 +6,13 @@ describe 'the_newslens_test' do
   before do
     @newsfound = Scarper::NewsLens.gets_news
   end
+
+  it 'they are not nil' do
+    @newsfound.each do |x|
+      x['title'].wont_be_nil
+      x['author'].wont_be_nil
+      x['date'].wont_be_nil
+    end
+  end
+
 end
